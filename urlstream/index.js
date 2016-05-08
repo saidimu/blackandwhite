71290471d8f983b64e790ee21cceb413f2077ca1
said apale <saidimu@gmail.com>
2016-05-07 23:10:18 -0400

    [ADD] urlstream: added nsqjs tweet-urls publishing

--- urlstream/index.js
+++ urlstream/index.js
@@ -1,3 +1,5 @@
+var nsq = require('nsqjs');
+
 var Twit = require('twit');
 
 var twitter = new Twit({
@@ -8,20 +10,30 @@ var twitter = new Twit({
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });//twitter
 
-var stream = twitter.stream('statuses/filter', {
-  track: process.env.TWITTER_STREAMING_TRACK_KEYWORDS,
-  language: process.env.TWITTER_STREAMING_LANGUAGE
-});// stream
+var writer = new nsq.Writer(process.env.NSQD_1_PORT_4150_TCP_ADDR, process.env.NSQD_1_PORT_4150_TCP_PORT);
+writer.connect();
 
-stream.on('tweet', function (tweet) {
-  console.log(tweet.text);
-  console.log(tweet.entities.urls[0].expanded_url);
-});// stream.on('tweet')
+writer.on('ready', function () {
+  var stream = twitter.stream('statuses/filter', {
+    track: process.env.TWITTER_STREAMING_TRACK_KEYWORDS,
+    language: process.env.TWITTER_STREAMING_LANGUAGE
+  });// stream
 
-stream.on('disconnect', function (disconnectMessage) {
-  console.error(disconnectMessage);
+  stream.on('tweet', function (tweet) {
+    // console.log(tweet.text);
+    console.log(tweet.entities.urls[0].expanded_url);
+    writer.publish(process.env.TWEET_URL_NSQ_TOPIC, tweet.entities.urls[0].expanded_url);
+  });// stream.on('tweet')
 
-});// stream.on('disconnect')
-stream.on('error', function (error) {
-  console.error(error);
-});// stream.on('error')
+  stream.on('disconnect', function (disconnectMessage) {
+    console.error(disconnectMessage);
+  });// stream.on('disconnect')
+
+  stream.on('error', function (error) {
+    console.error(error);
+  });// stream.on('error')
+});// writer.on('ready')
+
+writer.on('closed', function () {
+  console.warn('Writer closed');
+});// writer.on('closed')
