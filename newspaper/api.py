484c34ea4c55b0f0dd2d95e0c5fd74be951e0b8b
said apale <saidimu@gmail.com>
2016-05-10 15:49:19 -0400

    [FIX] newspaper: bugfix on Hug annotation types

--- newspaper/api.py
+++ newspaper/api.py
@@ -4,6 +4,6 @@ import hug
 from analysis import url_handler
 
 @hug.get('/top_image')
-def top_image(url:hug.types.longer_than(7, convert='text')):
+def top_image(url:hug.types.longer_than(7)):
     """Returns a URL's top image"""
     return url_handler(url)
