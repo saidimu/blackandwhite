a774300025916f743dbc3b3a90411d754fcfe99d
said apale <saidimu@gmail.com>
2016-05-11 15:47:21 -0400

    [IMP] urlstream: improved handling of fetch API call responses

--- urlstream/analysis.js
+++ urlstream/analysis.js
@@ -6,13 +6,17 @@ const port = process.env.NEWSPAPER_PORT_8000_TCP_PORT;
 const endpoint = `http://${host}:${port}/top_image`;
 // const endpoint = `${host}/top_image`;
 
-export async function get_top_image(url_object)  {
+export function get_top_image(url_object)  {
   try {
     const url = url_object[0].expanded_url || url_object[0].url;
-    const response = await fetch(`${endpoint}?url=${url}`);
-    const top_image = response.json();
-    console.log(top_image);
-    return top_image;
+    fetch(`${endpoint}?url=${url}`)
+      .then(function(response)  {
+        return response.text();
+      }).then(function(body)  {
+        const top_image = body;
+        console.log(top_image);
+        return top_image;
+      });// fetch
   } catch (e) {
     console.error(e);
     return null;
