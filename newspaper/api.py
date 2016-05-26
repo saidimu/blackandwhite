edbccece28cfb5fbf7f37bd7bc07718b8e9d4266
said apale <saidimu@gmail.com>
2016-05-26 00:17:03 -0400

    [IMP] newspaper: extracting more results from newspaper3k

--- newspaper/api.py
+++ newspaper/api.py
@@ -3,7 +3,7 @@ import hug
 
 from analysis import url_handler
 
-@hug.get('/top_image')
+@hug.get('/top_image', output=hug.output_format.json)
 def top_image(url:hug.types.longer_than(7)):
     """Returns a URL's top image"""
-    return url_handler(url)
+    return url_handler(url, nlp=True)
