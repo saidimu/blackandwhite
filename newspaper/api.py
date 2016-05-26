5916e6af98d1ce582d1220cb1c032ee3e8d54b18
said apale <saidimu@gmail.com>
2016-05-26 01:06:50 -0400

    [FIX] newspaper: stupid bug

--- newspaper/api.py
+++ newspaper/api.py
@@ -8,4 +8,4 @@ from analysis import url_handler
 @hug.get('/top_image', output=hug.output_format.json)
 def top_image(url:hug.types.longer_than(7)):
     """Returns a URL's top image"""
-    return url_handler(url, nlp=True)
+    return url_handler(url, True)
