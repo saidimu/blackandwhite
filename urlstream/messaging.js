51f7500998ca6eacddd08de104ab15e51dc98dd2
said apale <saidimu@gmail.com>
2016-05-27 18:28:03 -0400

    [ADD] urlstream: added logging to messaging.js

--- urlstream/messaging.js
+++ urlstream/messaging.js
@@ -1,5 +1,9 @@
 var nsq = require('nsqjs');
 
+var path = require('path');
+var appname = path.basename(__filename, '.js');
+var log = require('./logging.js')(appname);
+
 let reader;
 let writer;
 
@@ -12,18 +16,21 @@ export function init_writer(ready, error, closed) {
   writer.connect();
 
   ready = ready || function () {
-    console.log();
+    log.info({
+      nsqd_host, nsqd_port
+    }, "NSQ Writer ready.");
   };// ready
   writer.on('ready', ready);
 
   error = error || function (err) {
     console.error(err);
+    log.error({ err });
     process.exit(1);  // exit with an error so Docker can handle restarts
   };// error
   writer.on('error', error);
 
   closed = closed || function () {
-    console.warn('Writer closed');
+    log.warn({nsqd_host, nsqd_port}, 'Writer closed');
     process.exit(1);  // exit with an error so Docker can handle restarts
   };// closed
   writer.on('closed', closed);
@@ -32,7 +39,7 @@ export function init_writer(ready, error, closed) {
 export function publish(topic, message, callback) {
   callback = callback || function(err)  {
     if(err) {
-      console.error(err);
+      log.error({ err });
     }//if
   };// callback
 
@@ -47,7 +54,7 @@ export function init_reader(topic, channel, handlers) {
     maxInFlight: parseInt(process.env.NSQD_READER_MAX_IN_FLIGHT) || 10
   };// options
 
-  console.log("Reader options: %s", JSON.stringify(options));
+  log.info({options}, "NSQ Reader options");
 
   reader = new nsq.Reader(
     topic,
@@ -60,28 +67,32 @@ export function init_reader(topic, channel, handlers) {
   let { message, discard, error, nsqd_connected, nsqd_closed } = handlers;
 
   if(!message)  {
-    throw new Error('A "on message" handler was not specified!');
+    let err = new Error('A "on message" handler was not specified!');
+    log.error({err});
+    throw err;
   }//if
   reader.on('message', message);
 
   if(!discard)  {
-    throw new Error('A "on discard" handler was not specified!');
+    let err = new Error('A "on discard" handler was not specified!');
+    log.error({err});
+    throw err;
   }//if
   reader.on('discard', discard);
 
   error = error || function (err) {
-    console.error(err);
+    log.error({err});
     process.exit(1);  // exit with an error so Docker can handle restarts
   };// error
   reader.on('error', error);
 
   nsqd_connected = nsqd_connected || function (host, port) {
-    console.log(`NSQD Reader CONNECTED: ${host}:${port}`);
+    log.info({host, port}, "NSQD Reader CONNECTED");
   };// nsqd_connected
   reader.on('nqsd_connected', nsqd_connected);
 
   nsqd_closed = nsqd_closed || function (host, port) {
-    console.warn(`NSQD Reader CLOSED: ${host}:${port}`);
+    log.info({host, port}, "NSQD Reader CLOSED");
     process.exit(1);  // exit with an error so Docker can handle restarts
   };// nsqd_closed
   reader.on('nsqd_closed', nsqd_closed);
