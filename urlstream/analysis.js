0ee68c15c855770421054effb44552139c16d568
said apale <saidimu@gmail.com>
2016-05-26 23:37:07 -0400

    [FIX] urlstream: minor bugfix

--- urlstream/analysis.js
+++ urlstream/analysis.js
@@ -84,18 +84,7 @@ export function process_urls() {
           get_article(expanded_url)
             .then(function(article)  {
               const {
-                publish_date,
-                html,
-                title,
-                article,
-                source_url,
-                images,
-                authors,
-                text,
-                canonical_link,
-                movies,
-                keywords,
-                summary
+                title
               } = article;
 
               console.log(title);
