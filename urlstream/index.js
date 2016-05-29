e95a73b670e746f46b89c91944abdc9e40b3fc92
said apale <saidimu@gmail.com>
2016-05-29 00:25:43 -0400

    [ADD] urlstream: added initial steps to StatsD Datadog integration

--- urlstream/index.js
+++ urlstream/index.js
@@ -19,14 +19,23 @@ var path = require('path');
 var appname = path.basename(__filename, '.js');
 var log = require('./logging.js')(appname);
 
+var StatsD = require('hot-shots');
+var stats = new StatsD({
+  host: process.env.STATSD_PORT_8125_UDP_ADDR,
+  port: process.env.STATSD_PORT_8125_UDP_POR
+});
+// Catch socket errors so they don't go unhandled, as explained
+// in the Errors section below
+stats.socket.on('error', function(err) {
+  log.error({err}, "Error in STATSD socket: ");
+});// stats.socket
+
 init_writer();
 
 get_tweet_stream((tweet) => {
   const topic = process.env.TWEETS_TOPIC;
-  log.info({
-    tweet_id: tweet.id_str,
-    topic
-  }, 'Publishing received tweet.');
+  stats.increment('tweets');
+  log.info({ tweet_id: tweet.id_str, topic }, 'Publishing received tweet.');
   publish(topic, tweet);
 });// get_tweet_stream
 
