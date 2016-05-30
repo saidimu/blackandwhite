0f6b62c3322989e1f74be40dc4e591e468bb4dc4
said apale <saidimu@gmail.com>
2016-05-30 16:27:51 -0400

    [ADD] newspaper: added logging

--- newspaper/api.py
+++ newspaper/api.py
@@ -2,10 +2,12 @@
 import hug
 
 from analysis import article_handler
+from logging import loggly
 
 @hug.cli()
 @hug.local()
 @hug.get('/article', output=hug.output_format.json)
 def article(url:hug.types.longer_than(7)):
     """Returns a URL's article data"""
+    loggly.info("Analyzing '%s'", url)
     return article_handler(url, True)
