855a9f0424b92b963f34c5868be65b80e94d8c32
said apale <saidimu@gmail.com>
2016-05-30 18:32:34 -0400

    [ADD] newspaper: added DataDog statsd metrics library

--- newspaper/api.py
+++ newspaper/api.py
@@ -3,9 +3,11 @@ import hug
 
 from analysis import article_handler
 from logger import loggly
+from statsd import statsd
 
 @hug.cli()
 @hug.local()
+@statsd.timed('url_analysis.get_article')
 @hug.get('/article', output=hug.output_format.json)
 def article(url:hug.types.longer_than(7)):
     """Returns a URL's article data"""
