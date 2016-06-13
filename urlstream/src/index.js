6f2a259a9fc5816217de59ed5949e3c7217f1536
said apale <saidimu@gmail.com>
2016-06-13 11:51:44 -0400

    [IMP] urlstream: split out tweets-streaming into its own container

--- urlstream/src/index.js
+++ urlstream/src/index.js
@@ -1,12 +1,3 @@
-// import {
-//   get_tweet_stream
-// } from './twitter.js';
-//
-// import {
-//   init_writer,
-//   publish
-// } from './messaging.js';
-
 import {
   process_tweets,
   process_urls,
@@ -15,23 +6,6 @@ import {
   save_articles
 } from './analysis.js';
 
-// import {
-//   stats
-// } from './statsd.js';
-//
-// var path = require('path');
-// var appname = path.basename(__filename, '.js');
-// var log = require('./logging.js')(appname);
-//
-// init_writer();
-//
-// // each of the following methods could be its own container
-// get_tweet_stream((tweet) => {
-//   const topic = process.env.TWEETS_TOPIC;
-//   log.info({ tweet_id: tweet.id_str, topic }, 'Publishing received tweet.');
-//   stats.increment(`${topic}` + '.count');
-//   publish(topic, tweet);
-// });// get_tweet_stream
 process_tweets();
 process_urls();
 save_urls();
