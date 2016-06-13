4f773bca8175120d36e3fd16c008485956d771c7
said apale <saidimu@gmail.com>
2016-06-13 14:37:17 -0400

    [ADD] urlstream: split out save_urls into its own container

--- urlstream/src/index.js
+++ urlstream/src/index.js
@@ -1,13 +1,13 @@
 import {
-  process_tweets,
-  process_urls,
-  save_urls,
-  process_articles,
-  save_articles
+  // process_tweets,
+  // process_urls,
+  // save_urls,
+  // process_articles,
+  save_articles,
 } from './analysis.js';
 
 // process_tweets();
 // process_urls();
-save_urls();
+// save_urls();
 // process_articles();
 save_articles();
